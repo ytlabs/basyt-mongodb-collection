1cf436df77d9ea00d75c9708271aced25ba64d69
M. Kamil Sulubulut <kamil@yt.com.tr>
2015-05-19 00:07:55 +0300

    added search property

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ function MongoDBCollection(config, fileName) {
     this.name = config.name || fileName;
     this.storage = "mongodb";
     this.collection = MongoDB.collection(config.name);
-    var storageDefaultIdField = '_id', idField = storageDefaultIdField, idFunction = MongoDB.ObjectId;
+    var storageDefaultIdField = '_id',
+        idField = storageDefaultIdField,
+        idFunction = MongoDB.ObjectId,
+        textFields = [];
 
     _.forOwn(config.attributes, function (properties, field) {
         if (_.isString(properties)) return true;
+        //setup search
+
+        if (!_.isUndefined(properties.search)) {
+            textFields.push([field, properties.search])
+        }
         //setup index
         if (properties.index === true || properties.index === -1) {
 
@@ -40,6 +48,22 @@ function MongoDBCollection(config, fileName) {
         }
     });
 
+    if(textFields.length > 0) {
+        var index = {}, weights = {};
+        _.forEach(textFields, function(field) {
+            index[field[0]] = 'text';
+            if(_.isNumber(field[1])) {
+                weights[field[0]] = field[1];
+            }
+        });
+        if(_.isEmpty(weights)){
+            MongoDB.collection(config.name).createIndex(index, {name: "TextIndex"});
+        }
+        else {
+            MongoDB.collection(config.name).createIndex(index, {weights: weights, name: "TextIndex"});
+        }
+    };
+
     this.storageDefaultIdField = storageDefaultIdField;
     this.idField = idField;
     this.idFunction = idFunction;
@@ -51,5 +75,3 @@ function MongoDBCollection(config, fileName) {
 
 util.inherits(MongoDBCollection, BasytBaseCollection);
 MongoDBCollection.prototype.adapter = _.extend({}, BasytBaseCollection.adapter, adapter);
-
-
